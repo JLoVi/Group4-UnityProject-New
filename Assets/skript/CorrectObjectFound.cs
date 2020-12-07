ba48a5e954ad566617748534bc3aaf4fe91d04fb
jelena viskovic <jelena.viskovic@gmail.com>
2020-12-07 14:07:15 +0000

    correct object found interaction

---
+++ Assets/skript/CorrectObjectFound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CorrectObjectFound : MonoBehaviour
+{
+
+    public GameObject pointerLight;
+
+    public GameObject textObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointerLight.SetActive(false);
+
+        textObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        pointerLight.SetActive(true);
+
+        textObject.SetActive(true);
+    }
+
+    //if you want to make text disappear after some time look at COROUTINES online
+}
